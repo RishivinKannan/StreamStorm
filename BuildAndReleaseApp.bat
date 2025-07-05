2d4bb4ce65a8d079280654f9526678492d70e2e6
Ashif4354 <ashif.abhas@gmail.com>
2025-07-05 16:26:38 +0530

    Refactor build and deployment script to improve structure and readability

--- BuildAndReleaseApp.bat
+++ BuildAndReleaseApp.bat
@@ -2,7 +2,19 @@ cd Engine
 uv sync
 call .venv/Scripts/activate
 cd..
+
 auto-py-to-exe -c apte-ss-config.json
+
+cd Site
+vite build
+cd..
+
+cd UI
+vite build
+cd ..
+
+firebase deploy
+
 cd output
 dgupdater commit
 dgupdater publish
