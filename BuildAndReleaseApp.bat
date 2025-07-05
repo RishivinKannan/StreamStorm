289e91e7a72f2b5d4faebbf9cf4688e5bf4f328e
Ashif4354 <ashif.abhas@gmail.com>
2025-07-05 15:58:08 +0530

    Update configuration paths and enhance footer with links for better navigation

--- BuildAndReleaseApp.bat
+++ BuildAndReleaseApp.bat
@@ -2,7 +2,7 @@ cd Engine
 uv sync
 call .venv/Scripts/activate
 cd..
-auto-py-to-exe -c auto-py-to-exe-config.json
+auto-py-to-exe -c apte-ss-config.json
 cd output
 dgupdater commit
 dgupdater publish
--- Site/src/Components/Footer.jsx
+++ Site/src/Components/Footer.jsx
@@ -1,9 +1,12 @@
-
 const Footer = () => (
     <footer className="footer">
+        <div className="footer-links">
+            <a href="/tc" className="footer-link">Terms and Conditions</a>
+            <a href="/pp" className="footer-link">Privacy Policy</a>
+        </div>
+        <br />
         © 2024 StreamStorm. All rights reserved.
     </footer>
 );
 
 export default Footer;
-
