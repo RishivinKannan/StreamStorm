c97b404142286d4ee642ef9d3f77f6b20eb7eea7
A Ashif <ashif.abhas@gmail.com>
2025-10-18 14:09:56 +0530

    fix(api): enhance validation error messages and logging
    Improve error handling in validation exceptions by adding a field mapping for user-friendly messages and including debug logging. Update response to provide custom message and error context for better debugging.

--- src/Engine/StreamStorm/api/lib/exception_handlers.py
+++ src/Engine/StreamStorm/api/lib/exception_handlers.py
@@ -19,13 +19,40 @@ async def common_exception_handler(request: Request, exc: Exception) -> JSONResp
     
     
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors = exc.errors()
+    logger.debug(f"Validation errors: {errors}")
+    
+    field_map: dict[str, str] = {
+        "slow_mode": "Slow mode",
+        "slowMode": "Slow mode",
+        "messages": "Messages",
+        "video_url": "Video URL",
+        "videoUrl": "Video URL",
+        "chat_url": "Chat URL",
+        "chatUrl": "Chat URL",
+        "subscribe": "Subscribe",
+        "subscribe_and_wait": "Subscribe and wait",
+        "subscribeAndWait": "Subscribe and wait",
+        "subscribe_and_wait_time": "Subscribe and wait time",
+        "subscribeAndWaitTime": "Subscribe and wait time",
+        "channels": "Channels",
+        "background": "Background",
+        "count": "Profiles count",
+        "mode": "Channels selection mode"
+    }
+    
+    message: str = field_map[errors[0]["loc"][1]] + " : " + errors[0]["msg"][6:].capitalize()
+    
+    if "ctx" in errors[0]:
+        del errors[0]["ctx"]
 
     return JSONResponse(
         status_code=422,
         content={
             "success": False,
-            "message": exc.errors()[0]["msg"],
-        },
+            "message": message,
+            "context": errors
+        }
     )
     
     
--- src/UI/src/context/StormDataContext.jsx
+++ src/UI/src/context/StormDataContext.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
 import { createContext, useState, useRef, useContext } from 'react';
 import { useNotifications } from "@toolpad/core/useNotifications";
 import { useLocalStorageState } from "@toolpad/core/useLocalStorageState";
