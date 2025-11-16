f55208b8734e6bf1a507d8f2daf2adb022de8006
A Ashif <ashif.abhas@gmail.com>
2025-11-16 07:11:26 +0530

    build(linux): update runner and enhance artifact download script
    - Change CI workflow to use ubuntu-22.04 runner
    - Refactor download script to use pathlib, add logging, and move artifacts to export directory
    - Update download directory from native-builds to linux-builds

--- build/scripts/download_linux_artifacts.py
+++ build/scripts/download_linux_artifacts.py
@@ -1,10 +1,21 @@
-from subprocess import run, CompletedProcess
-import json
-from logging import basicConfig, INFO, info as log_info
+from pathlib import Path
+ROOT: Path = Path(__file__).parent.parent.parent.resolve()
+
+from sys import path  # noqa: E402
+path.insert(0, str(ROOT))
+
+from subprocess import run, CompletedProcess  # noqa: E402
+from json import loads  # noqa: E402
+from shutil import move, rmtree  # noqa: E402
+from logging import basicConfig, INFO, info as log_info  # noqa: E402
 
 basicConfig(level=INFO)
 
+log_info(f"Root directory: {ROOT}")
+
 def get_latest_run_id() -> str:
+    
+    log_info("Getting latest run id...")
     
     cmd: list[str] = [
         "gh", "run", "list",
@@ -16,7 +27,7 @@ def get_latest_run_id() -> str:
 
     result: CompletedProcess[str] = run(cmd, capture_output=True, text=True, check=True)
     
-    if data := json.loads(result.stdout):
+    if data := loads(result.stdout):
         log_info("Latest run id: %s", data[0]["databaseId"])
         return data[0]["databaseId"]
     
@@ -24,19 +35,53 @@ def get_latest_run_id() -> str:
         raise RuntimeError("Bruh, No runs found")
 
 
-def download_run_artifacts(run_id) -> None:
+def download_run_artifacts(run_id, streamstorm_deb: Path, streamstorm_tar: Path) -> None:
+    
+    log_info("Downloading artifacts...")
+    
+    if streamstorm_deb.exists():
+        streamstorm_deb.unlink()
+    if streamstorm_tar.exists():
+        streamstorm_tar.unlink()
     
     cmd: list[str] = [
         "gh", "run", "download",
         str(run_id),
         "--repo", "Ashif4354/StreamStorm",
-        "--dir", "./native-builds"
+        "--dir", "./linux-builds"
     ]
 
     run(cmd, check=True)
     log_info("Artifacts downloaded")
+    
+    
+def move_artifacts(streamstorm_deb: Path, streamstorm_tar: Path) -> None:
+    
+    deb_destination: Path = ROOT / "export" / "installers" / "streamstorm.deb"
+    tar_destination: Path = ROOT / "export" / "installers" / "streamstorm.tar.gz"
+    
+    if deb_destination.exists():
+        deb_destination.unlink()
+    if tar_destination.exists():
+        tar_destination.unlink()
+    
+    move(str(streamstorm_deb), str(ROOT / "export" / "installers" / "streamstorm.deb"))
+    move(str(streamstorm_tar), str(ROOT / "export" / "installers" / "streamstorm.tar.gz"))
+    
+    rmtree(str(ROOT / "linux-builds"))
 
+
+def main() -> None:
+    
+    streamstorm_deb: Path = ROOT / "linux-builds" / "streamstorm-deb" / "streamstorm.deb"
+    streamstorm_tar: Path = ROOT / "linux-builds" / "streamstorm-tar" / "streamstorm.tar.gz"
+    
+    run_id: str = get_latest_run_id()
+    
+    download_run_artifacts(run_id, streamstorm_deb, streamstorm_tar)
+    
+    move_artifacts(streamstorm_deb, streamstorm_tar)
 
 if __name__ == "__main__":
-    run_id: str = get_latest_run_id()
-    download_run_artifacts(run_id)
+    main()
+    
